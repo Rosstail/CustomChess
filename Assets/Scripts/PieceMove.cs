ec4ce28738827a1e30a22fa3491f0f891e88e569
Rosstail <rosstail_zegeek@hotmail.fr>
2022-10-26 16:45:01 +0200

    Added check for attack normal ranges and checked obstacles

--- Assets/Scripts/PieceMove.cs
+++ Assets/Scripts/PieceMove.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PieceMove : MonoBehaviour
 {
@@ -77,16 +78,96 @@ public class PieceMove : MonoBehaviour
     {
         ChessCase selectedChessCase = selectedCaseTransform.GetComponent<ChessCase>();
         ChessCase targetedChessCase = targetedCaseTransform.GetComponent<ChessCase>();
-        Piece selectedPawn = selectedChessCase.currentPiece.GetComponent<Piece>();
-        Piece targetPawn = targetedChessCase.currentPiece.GetComponent<Piece>();
+        Piece selectedPiece = selectedChessCase.currentPiece.GetComponent<Piece>();
+        Piece targetPiece = targetedChessCase.currentPiece.GetComponent<Piece>();
 
-        if (selectedPawn.team == targetPawn.team)
+        if (selectedPiece.team == targetPiece.team || !CheckAttackMove())
         {
             targetedCaseTransform = null;
             return;
         }
+        AttackPiece();
+    }
+
+    bool CheckAttackMove()
+    {
+        foreach (Transform attackRange in GetAttackCase())
+        {
+            if (targetedCaseTransform == attackRange && CheckObstacle())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    List<Transform> GetAttackCase()
+    {
+        List<Transform> ranges = new List<Transform>();
+        Transform currentPiece = selectedCaseTransform.GetComponent<ChessCase>().currentPiece;
+        Piece selectedPiece = currentPiece.GetComponent<Piece>();
+        for (int i = 0; i < selectedPiece.simpleAttacks.Count; i++)
+        {
+            Vector2 simpleAttack = selectedPiece.simpleAttacks[i];
+            Vector3 attackPos = selectedCaseTransform.position;
+
+            if (currentPiece.localEulerAngles.y == 180)
+            {
+                attackPos.x = attackPos.x - simpleAttack.x;
+            } else
+            {
+                attackPos.x = attackPos.x + simpleAttack.x;
+            }
+
+            attackPos.z = attackPos.z + simpleAttack.y;
+
+            Collider[] hitColliders = Physics.OverlapSphere(attackPos, 0.1f, layerMask);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.tag == "Case")
+                {
+                    ranges.Add(hitCollider.transform);
+                }
+            }
+        }
+
+        return ranges;
+    }
+
+    bool CheckObstacle()
+    {
+        Transform currentPiece = selectedCaseTransform.GetComponent<ChessCase>().currentPiece;
+        Piece selectedPiece = currentPiece.GetComponent<Piece>();
+        if (selectedPiece.canJumpPieces)
+        {
+            return true;
+        }
+
+        Vector3 direction = targetedCaseTransform.position - selectedCaseTransform.position;
+        RaycastHit[] hits = Physics.RaycastAll(selectedCaseTransform.position, direction);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            ChessCase chessCase = hit.transform.GetComponent<ChessCase>();
+            if (hit.transform != selectedCaseTransform && hit.transform != targetedCaseTransform)
+            {
+                if (chessCase.currentPiece != null)
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+    void AttackPiece()
+    {
+        ChessCase selectedChessCase = selectedCaseTransform.GetComponent<ChessCase>();
+        ChessCase targetedChessCase = targetedCaseTransform.GetComponent<ChessCase>();
+        Piece targetPiece = targetedChessCase.currentPiece.GetComponent<Piece>();
         selectedChessCase.currentPiece.position = targetedChessCase.currentPiece.position;
-        targetPawn.Taken();
+        targetPiece.Taken();
         selectedChessCase.CheckPawnTransform();
         targetedChessCase.CheckPawnTransform();
         Unselect();
